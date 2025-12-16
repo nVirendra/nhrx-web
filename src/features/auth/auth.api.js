5bdf925c16dcd39e250fbb5a08cb37a05fd7a764
nVirendra <virendran818@gmail.com>
2025-12-17 02:50:07 +0530

    api call fix

--- src/features/auth/auth.api.js
+++ src/features/auth/auth.api.js
@@ -4,7 +4,7 @@ import { useMutation, useQuery  } from "@tanstack/react-query";
 export const useLogin = () => {
   return useMutation({
     mutationFn: async (payload) => {
-      const res = await api.post("/v1/auth/login", payload);
+      const res = await api.post("/api/v1/auth/login", payload);
       return res.data;
     },
   });
@@ -15,7 +15,7 @@ export const useMe = () => {
   return useQuery({
     queryKey: ["me"],
     queryFn: async () => {
-      const res = await api.get("/v1/auth/me");
+      const res = await api.get("/api/v1/auth/me");
       return res.data.user;
     },
     retry: false,
