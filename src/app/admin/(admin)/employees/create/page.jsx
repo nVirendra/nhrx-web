84092bed034743ec3c897f21e3875eea9923b3c3
nVirendra <virendran818@gmail.com>
2026-01-04 14:07:46 +0530

    fix employee create

--- src/app/admin/(admin)/employees/create/page.jsx
+++ src/app/admin/(admin)/employees/create/page.jsx
@@ -1,32 +1,10 @@
-"use client";
+import { Suspense } from "react";
 
-import React from "react";
-import { useSearchParams } from "next/navigation";
-
-
-import EmployeeFormTabs from "../add/page";
-import { useEmployeeDetails } from "@/features/employee/employee.api";
-
-const EmployeeCreateEditPage = () => {
-  const searchParams = useSearchParams();
-  const editId = searchParams.get("edit");
-
-  const isEdit = Boolean(editId);
-
-  const { data, isLoading } = useEmployeeDetails(
-    isEdit ? Number(editId) : null
-  );
-
-  if (isEdit && isLoading) {
-    return <p className="p-6">Loading employee details...</p>;
-  }
-
+import EmployeeCreateEditClient from "../components/EmployeeCreateEditClient";
+export default function Page() {
   return (
-    <EmployeeFormTabs
-      mode={isEdit ? "edit" : "create"}
-      employeeData={data?.data || {}}
-    />
+    <Suspense fallback={<p className="p-6">Loading...</p>}>
+      <EmployeeCreateEditClient />
+    </Suspense>
   );
-};
-
-export default EmployeeCreateEditPage;
+}
